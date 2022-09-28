8c751acc40f6593428dab7e12ca80a87d575badc
chobkyu <qudqud97@naver.com>
2022-09-29 00:27:36 +0900

    기본 클라이언트 헤더

---
+++ client/src/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+function Header(){
+    return (
+        <div className="container">
+        <header>
+          <nav>
+            <div className="icon">TODO List</div>
+            <div className="gnb">
+              <Link to="/">메인페이지</Link>
+              <Link to="/write">글쓰기</Link>
+              <Link to="/login">로그인</Link>
+            </div>
+          </nav>
+        </header>
+      </div>
+    )
+}
+
+export default Header;
